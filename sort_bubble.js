27dc6a1329d1cfd7b0537b04a335ccd5aa5bc3a8
shubham.bhatt <shubham.bhatt@kelltontech.com>
2021-04-26 09:49:59 +0530

    bubble sort changes

--- sort_bubble.js
+++ sort_bubble.js
@@ -1,7 +1,7 @@
 function bubbleSort(arr) {
     let noSwaps = true
     for (let i = arr.length; i > 0; i--) {
-        for (let j = 0; j < arr.length; j++) {
+        for (let j = 0; j < i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j + 1]
                 arr[j + 1] = arr[j]
