1a73e6436da08a4c46879450a39ee9c2b6f450c6
shubham.bhatt <shubham.bhatt@kelltontech.com>
2021-05-12 09:52:16 +0530

    insertion sort implemented

---
+++ sort_insertion.js
@@ -0,0 +1,13 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            }
+        }
+    }
+    return arr
+}
+console.log(insertionSort([2,3,1,8,4,6]))
